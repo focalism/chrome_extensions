146261f3682f16092e6047636e8c2db945b4966d
xuzg <xzg.nju@gmail.com>
2018-06-14 23:45:29 +0800

    computer monitor

--- computer_monitor/background.js
+++ computer_monitor/background.js
@@ -1,11 +1,17 @@
-chrome.app.runtime.onLaunched.addListener(function(){
-    chrome.app.window.create("main.html"),{
-        'id':'main',
-        'bounds':{
-            'width':542,
-            'height':360,
-        },
-        'resizeable':false,
-        'frame':'none'
+chrome.app.runtime.onLaunched.addListener(function() {
+    var main_window = chrome.app.window.get('main');
+    if(main_window){
+        main_window.show();
     }
-})
+    else{
+        chrome.app.window.create('main.html', {
+            //'id': 'main',
+            'bounds': {
+                'width': 542,
+                'height': 360
+            },
+            'resizable': false,
+            'frame': 'none'
+        });
+    } 
+});
