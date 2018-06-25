5b97372d496871d3b1cec027ddccec4e934e9512
xuzg <xzg.nju@gmail.com>
2018-06-25 22:54:11 +0800

    media manager

--- media_manager/main.js
+++ media_manager/main.js
@@ -30,7 +30,7 @@ document.getElementById('home').onclick = function(){
     getMedia()
 }
 
-chrome.mediaGalleries.onScaProgaress&&chrome.mediaGalleries.onScaProgaress.addListener(function(details){
+chrome.mediaGalleries.onScanProgaress&&chrome.mediaGalleries.onScaProgaress.addListener(function(details){
     switch(details.type){
         case 'start':
             scanning = true;
@@ -62,67 +62,23 @@ function getMedia(){
 function listMediaGalleries(fileSystemArray){
     document.getElementById('container').innerHTML = ''
     for(var i = 0; i<fileSystemArray.length;i++){
-        (function temp(fs){
-            var info = chrome.mediaGalleries.getMediaFileSystemMetadata(fs);
-            var item = document.createElement('span');
-            item.className = 'item';
-            item.title = info.name;
-            item.onclick = function(){
-                searching = true;
-                showMedia(fs.root,this.title)
-            }
-            var iconfont = document.createElement('i')
-            iconfont.className = "iconfont"
-            iconfont.innerHTML = '&#xf00c5;';
-            item.appendChild(iconfont)
-            document.getElementById('container').appendChild(item);
-            var text = document.createElement("span");
-            text.className = 'text';
-            text.innerHTML = info.name;
-            item.appendChild(text);
-        })(fileSystemArray[i])
-    }
-}
-function listMedia(Entries){
-    for(var i = 0;i<Entries.length;i++){
-        if(!searching){
-            break;
-        }
-        if(Entries[i].isFile){
-            var ext = Entries[i].name.substr(Entries[i].name.lastIndexOf('.')+1).tolowerCase();
-            var type = (
-                imgFormats.indexOf(ext)>=0?'images':(
-                    vidFormats.indexOf(ext)>=0?'video':(
-                        audFormats.indexOf(ext)>=0?'music':null)))
-            if(!type){
-                continue;
-            }
-            var item = document.createElement('span')
-            itme.className = 'item'
-            item.title = Entries[i].name;
-            item.onclick = (function(Entry){})(Entries[i])
-            document.getElementById('container').appendChild(item);
-            var iconfont = document.createElement('i')
-            iconfont.className = 'iconfont';
-            iconfont.innerHTML = (type=='images'?'&#xf0137;':(type == 'video'?'&#xf0162;':'&#xf0036;'));
-            item.appendChild(iconfont);
-            var text = document.createElement('span');
-            text.className = 'text';
-            text.innerHTML = Entries[i].name;
-            item.appendChild(text);
-        }
-        else if(Entries[i].isDirectory){
-            var dirReader = Entries[i].createReader();
-            dirReader.readEntries(listMedia)
-        }
+        var info = chrome.mediaGalleries.getMediaFileSystemMetadata(fileSystemArray[i]);
+        var item = document.createElement('span');
+        item.className = 'item';
+        item.title = info.name;
+        var iconfont = document.createElement('i')
+        iconfont.className = "iconfont"
+        iconfont.innerHTML = '&#xf00c5;';
+        item.appendChild(iconfont)
+        document.getElementById('container').appendChild(item);
+        var text = document.createElement("span");
+        text.className = 'text';
+        text.innerHTML = info.name;
+        item.appendChild(text);
     }
 }
 
-function showMedia(Entry,path){
-    document.getElementById('container').innerHTML = '';
-    document.getElementById('subpath').innerHTML = '<span class="name">'+path+'</span><span class="pointer">»</span>'
-    var dirReader = Entry.createReader();
-    dirReader.readEntries(listMedia);
-}
+
+function listMedia()
 
 getMedia()
