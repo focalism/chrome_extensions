b48c24c5fd281797642ae07c3cb800245b408ae8
xuzg <xzg.nju@gmail.com>
2018-08-01 17:00:26 +0800

    modify elementfrompoint x y

--- get_selector/js/get_selector.js
+++ get_selector/js/get_selector.js
@@ -2,13 +2,13 @@ function get_selector(e){
     if(!e){
         e = window.event;
     }
-    console.log(e.clientX)
-    var oPoint=document.elementFromPoint(2,2); 
-    console.log(oPoint)
-    selector_div = document.getElementById("selector_div");
-    selector_div.innerHTML = "haha";
+
+    var oPoint=document.elementFromPoint(e.clientX,e.clientY);
+    var nodeName = oPoint.nodeName;
+
+    alert(nodeName);
 }
 
 
-document.onmousemove = get_selector;
+document.onmousedown = get_selector;
 
