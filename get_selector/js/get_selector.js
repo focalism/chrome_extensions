1b67c440519320c4cc007538130c73acfdb193ee
xuzg <xzg.nju@gmail.com>
2018-08-08 20:36:24 +0800

    get selector with parent node

--- get_selector/js/get_selector.js
+++ get_selector/js/get_selector.js
@@ -1,12 +1,11 @@
-
 function combo(arr, num) {
-    var result = [];
-    var range = function(r, _arr) {
+    let  result = [];
+    let  range = function(r, _arr) {
       if (r.length == num) {
         result.push(r);
       } else {
-        var l = r.length;
-        for (var i = 0, len = _arr.length - num + l; i <= len; i++) {
+        let  l = r.length;
+        for (let  i = 0, len = _arr.length - num + l; i <= len; i++) {
           range(r.concat(_arr[i]), _arr.slice(i + 1));
         }
       }
@@ -16,7 +15,7 @@ function combo(arr, num) {
   }
 
 function ifUnique(cssSelector){
-    selector = document.querySelectorAll(cssSelector);
+    let selector = document.querySelectorAll(cssSelector);
     if(selector.length==1){
         return true;
     }else{
@@ -25,96 +24,147 @@ function ifUnique(cssSelector){
 }
 
 function getAttributesFormat(attributeList){
-    attrNameValueList = [];
-    for(var attr of attributeList){
-        attrNameValueList.push(`[${attr.name}="${attr.value}"]`)
+    let attrNameValueList = [];
+    for(let  attr of attributeList){
+        attrNameValueList.push(`[${attr.name}="${attr.value}"]`);
     }
     return attrNameValueList;
 }
 
 function getSelectorByNodeName(nodeName){
     if(ifUnique(nodeName)){
-        return nodeName;
+        return [true, nodeName];
+    }else{
+        return [false, nodeName];
     }
-
 }
+
 function getSelectorById(idName){
-    selectorName = '#' + idName;
+    let selectorName = '#' + idName;
     if(ifUnique(selectorName)){
-        return selectorName;
+        return [true,selectorName];
+    }else{
+        return [false, selectorName]
     }
 }
 
 function getSelectorByClass(nodeName, className){
-    classList = className.split(' ');
+    let classList = className.split(' ');
+    let classSlectorList = []
     if(classList.length > 0){
-        for(i=1;i<=classList.length;i++){
-            classPartList = combo(classList, i);
-            for(var item of classPartList){
-                selectorName = nodeName + '.' + item.join('.');
+        for(let i=1;i<=classList.length;i++){
+            let classPartList = combo(classList, i);
+            for(let  item of classPartList){
+                let selectorName = nodeName + '.' + item.join('.');
+                classSlectorList.push(selectorName)
                 if(ifUnique(selectorName)){
-                    return  selectorName;
+                    return  [true, classSlectorList];
                 }
             }
         }
     }
+    return [false, classSlectorList]
 }
 
 function getSelectorByAttrName(nodeName, nameAttr){
+    let cssSelector =  nodeName + `[name="${nameAttr}"]`
     if(document.getElementsByName(nameAttr)){
-        return nodeName + `[name="${nameAttr}"]`;
+        return [true, cssSelector];
+    }else{
+        return [false, cssSelector]
     }
 }
 
 function getSelectorByAttr(nodeName, attrNameValueList){
-    console.log(attrNameValueList);
-    for(var j = 1;j<=attributes.length;j++){
-        attributePartList = combo(attrNameValueList, j);
-        console.log(attributePartList);
-        for(var item of attributePartList){
-            selectorName = nodeName + item.join('');
+    let attrSlectorList = []
+    for(let j = 1;j<=attrNameValueList.length;j++){
+        let attributePartList = combo(attrNameValueList, j);
+        for(let  item of attributePartList){
+            let selectorName = nodeName + item.join('');
+            attrSlectorList.push(selectorName)
             if(ifUnique(selectorName)){
-                return selectorName;
+                return [true, attrSlectorList];
             }
         }
-
     }
+    return [false, attrSlectorList]
 }
 
-function getSelector(el){
-    attributes = el.attributes;
-    attrNameValueList = getAttributesFormat(attributes);
-    var nodeName = el.nodeName.toLowerCase();
-    var cssSelector  = getSelectorByNodeName(nodeName);
+let allSelectorDict = {}
+let parentId = 0
+function getSelector(el, parent =false){
+    let selectorList = []
+    let attributes = el.attributes;
+    let attrNameValueList = getAttributesFormat(attributes);
+    let nodeName = el.nodeName.toLowerCase();
+    let result  = getSelectorByNodeName(nodeName);
+    let status = result[0]
+    let cssSelector;
+    selectorList.push(result[1])
     if(attributes.length > 0){
-        if(!cssSelector && attributes.id){
-            cssSelector = getSelectorById(attributes.id.value);
+        if(!status && attributes.id){
+            let result = getSelectorById(attributes.id.value);
+            status = result[0]
+            selectorList.push(result[1])
         }
-        if(!cssSelector && attributes.class){
-            cssSelector = getSelectorByClass(nodeName, attributes.class.value);
+        if(!status && attributes.name){
+            result = getSelectorByAttrName(nodeName, attributes.name.value);
+            status = result[0]
+            selectorList.push(result[1])
         }
-        if(!cssSelector && attributes.name){
-            cssSelector = getSelectorByAttrName(nodeName, attributes.name.value);
+        if(!status && attributes.class){
+            result= getSelectorByClass(nodeName, attributes.class.value);
+            status = result[0]
+            selectorList = selectorList.concat(result[1])
         }
-        if(!cssSelector){
-            cssSelector =  getSelectorByAttr(nodeName, attrNameValueList);
+        if(!status){
+            result =  getSelectorByAttr(nodeName, attrNameValueList);
+            status = result[0]
+            selectorList = selectorList.concat(result[1])
         }
     }
-    if(!cssSelector){
-        parentNode = el.parentNode;
-        console.log(parentNode.nodeName);
-        console.log(parentNode.attributes);
-        cssSelector = getSelector(parentNode);
+    if(!parent){
+        allSelectorDict["self"] = selectorList
+    }else{
+        allSelectorDict[`parent${parentId}`] = selectorList
     }
-    return cssSelector;
-}
+
+    console.log(parentId, status)
+    if(!status){
+        parentId += 1
+        let parentNode = el.parentNode;
+        console.log(parentNode)
+        getSelector(parentNode, parent=parentId);
+    }
+
+    let selfSelectorList =  allSelectorDict['self']
+    cssSelector = selfSelectorList[selfSelectorList.length-1]
+    if(ifUnique(cssSelector)){
+        return cssSelector
+    }
+
+
+    let nodeLength = Object.keys(allSelectorDict).length;
+    let uniqueParentSelectorList = allSelectorDict[`parent${nodeLength-1}`]
+    let uniqueParentSelector = uniqueParentSelectorList[uniqueParentSelectorList.length - 1]
+    for(selector of selfSelectorList){
+        cssSelector = uniqueParentSelector + ' ' + selector
+        if(ifUnique(cssSelector)){
+            return cssSelector
+        }
+    }
+
+    
+    }
 
 function get_selector(){
-    var e = event || window.event;
-    var elementFromPoint = document.elementFromPoint(e.clientX, e.clientY);
-    cssSelector = getSelector(elementFromPoint)
-    if(cssSelector){
-        alert(cssSelector);
+    allSelectorDict = {}
+    parentId = 0
+    let  e = event || window.event;
+    let  elementFromPoint = document.elementFromPoint(e.clientX, e.clientY);
+    let selector = getSelector(elementFromPoint);
+    if(selector){
+        alert(selector);
     }else{
         alert('cant not find the selector');
     }   
